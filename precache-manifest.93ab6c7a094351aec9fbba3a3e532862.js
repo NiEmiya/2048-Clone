da8d845eae2b760087be4ccf916f75c83c70b9ff
cybernaga <45783152+NiEmiya@users.noreply.github.com>
2022-06-22 16:02:36 -0400

    Updates

---
+++ precache-manifest.93ab6c7a094351aec9fbba3a3e532862.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa69922ff3c65030cd5fe0787c0f898c",
+    "url": "/2048-Clone/index.html"
+  },
+  {
+    "revision": "207bce275e0566fd1436",
+    "url": "/2048-Clone/static/css/main.9d0f99b7.chunk.css"
+  },
+  {
+    "revision": "e5016b9b35583d205211",
+    "url": "/2048-Clone/static/js/2.293da6e7.chunk.js"
+  },
+  {
+    "revision": "5f58498292eeb3ef40f62e38945025c2",
+    "url": "/2048-Clone/static/js/2.293da6e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "207bce275e0566fd1436",
+    "url": "/2048-Clone/static/js/main.4297923a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/2048-Clone/static/js/main.4297923a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50026a35cf99b50b7010",
+    "url": "/2048-Clone/static/js/runtime-main.aeb02157.js"
+  },
+  {
+    "revision": "e32dde8607904b31ca633a6f101715be",
+    "url": "/2048-Clone/static/media/grid.e32dde86.svg"
+  },
+  {
+    "revision": "509ae2e4853e375295e29a09bb916116",
+    "url": "/2048-Clone/static/media/restart.509ae2e4.svg"
+  },
+  {
+    "revision": "e263bfe98e5bbf96976110700709f636",
+    "url": "/2048-Clone/static/media/undo.e263bfe9.svg"
+  }
+]);
