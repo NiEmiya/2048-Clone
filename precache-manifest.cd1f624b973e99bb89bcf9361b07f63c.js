55a947f724332388b681a18cccc9684b241430d3
cybernaga <45783152+NiEmiya@users.noreply.github.com>
2022-06-22 15:46:14 -0400

    Updates

---
+++ precache-manifest.cd1f624b973e99bb89bcf9361b07f63c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9066559037e948c4f09759f3205c81a",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "29718e532433ee65d5a8",
+    "url": "/react-gh-pages/static/css/main.35eee826.chunk.css"
+  },
+  {
+    "revision": "4c73a2807248a67a22df",
+    "url": "/react-gh-pages/static/js/2.32ab35b9.chunk.js"
+  },
+  {
+    "revision": "82e8035e49740f791fa94175bc04ac4e",
+    "url": "/react-gh-pages/static/js/2.32ab35b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29718e532433ee65d5a8",
+    "url": "/react-gh-pages/static/js/main.c1b80636.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/react-gh-pages/static/js/main.c1b80636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04b48d74a39772ea7d5e",
+    "url": "/react-gh-pages/static/js/runtime-main.c016361f.js"
+  },
+  {
+    "revision": "e32dde8607904b31ca633a6f101715be",
+    "url": "/react-gh-pages/static/media/grid.e32dde86.svg"
+  },
+  {
+    "revision": "509ae2e4853e375295e29a09bb916116",
+    "url": "/react-gh-pages/static/media/restart.509ae2e4.svg"
+  },
+  {
+    "revision": "e263bfe98e5bbf96976110700709f636",
+    "url": "/react-gh-pages/static/media/undo.e263bfe9.svg"
+  }
+]);
